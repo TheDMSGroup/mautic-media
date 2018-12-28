6e42ba34a6323fa0f611dd530d9367e7a12f9e50
heathdutton <hdutton@thedmsgrp.com>
2018-12-28 10:12:00 -0500

    [ENG-502] FB Stub added.

--- Helper/FacebookHelper.php
+++ Helper/FacebookHelper.php
@@ -11,6 +11,11 @@
 
 namespace MauticPlugin\MauticMediaBundle\Helper;
 
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\AdsInsights;
+use FacebookAds\Api;
+use FacebookAds\Logger\CurlLogger;
+
 /**
  * Class FacebookHelper.
  *
@@ -22,28 +27,54 @@ class FacebookHelper
     /** @var \Facebook\Facebook */
     private $client;
 
-    /**
-     * FacebookHelper constructor.
-     *
-     * @param $client_id
-     * @param $client_secret
-     * @param $token
-     *
-     * @throws \Facebook\Exceptions\FacebookSDKException
-     */
+    /** @var @var AdUser */
+    private $user;
+
     public function __construct($client_id, $client_secret, $token)
     {
-        $this->client = new \Facebook\Facebook(
-            [
-                'app_id'                => $client_id,
-                'app_secret'            => $client_secret,
-                'default_graph_version' => 'v3.2',
-                'default_access_token'  => $token,
-            ]
-        );
+        Api::init($client_id, $client_secret, $token);
+        $this->client = Api::instance();
+        $this->client->setLogger(new CurlLogger());
     }
 
     public function pullData()
     {
+        $fields = [
+            'spend',
+            'cost_per_result',
+            'cpp',
+            'cpm',
+            'actions:rsvp',
+            'cost_per_action_type:checkin',
+            'cost_per_action_type:receive_offer',
+            'cost_per_action_type:rsvp',
+            'cost_per_action_type:photo_view',
+            'cost_per_action_type:post',
+            'cost_per_action_type:post_reaction',
+            'cost_per_action_type:post_engagement',
+            'cost_per_action_type:comment',
+            'cost_per_action_type:like',
+            'cost_per_action_type:page_engagement',
+            'campaign_group_id',
+            'campaign_id',
+            'adgroup_id',
+            'adgroup_name',
+            'account_id',
+            'account_name',
+            'campaign_group_name',
+            'campaign_name',
+            'schedule',
+        ];
+        $params = [
+            'level'      => 'ad',
+            'filtering'  => [],
+            'breakdowns' => ['days_1', 'hourly_stats_aggregated_by_advertiser_time_zone', 'ad_id'],
+            'time_range' => ['since' => '2018-12-27', 'until' => '2018-12-28'],
+        ];
+        // echo json_encode((new AdAccount($ad_account_id))->getInsights(
+        //     $fields,
+        //     $params
+        // )->getResponse()->getContent(), JSON_PRETTY_PRINT);
+
     }
 }
