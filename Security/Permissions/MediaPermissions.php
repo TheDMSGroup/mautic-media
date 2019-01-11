9f7bf150ede1760c16545d1a42a187bee40e5218
heathdutton <hdutton@thedmsgrp.com>
2019-01-11 14:20:25 -0500

    Correct permissions and readme.

---
+++ Security/Permissions/MediaPermissions.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Digital Media Solutions, LLC
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticMediaBundle\Security\Permissions;
+
+use Mautic\CoreBundle\Security\Permissions\AbstractPermissions;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class MediaPermissions.
+ */
+class MediaPermissions extends AbstractPermissions
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($params)
+    {
+        parent::__construct($params);
+        $this->addStandardPermissions('categories');
+        $this->addExtendedPermissions('items');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'media';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface &$builder, array $options, array $data)
+    {
+        $this->addStandardFormFields('media', 'categories', $builder, $data);
+        $this->addExtendedFormFields('media', 'items', $builder, $data);
+    }
+}
