ad844061b2b853cd302dbe943aff19de6c7b755b
Dean Martin <contact@deanmartin.pro>
2019-04-22 14:24:43 -0400

    [ENG-759] Added transparency to provider colors.

--- Report/CostBreakdownChart.php
+++ Report/CostBreakdownChart.php
@@ -18,28 +18,28 @@ class CostBreakdownChart
      */
     private $providerColors = [
         'facebook' => [
-            'backgroundColor'           => 'rgba(59,89,153 ,1 )',
-            'borderColor'               => 'rgba(59,89,153 ,1 )',
-            'pointHoverBackgroundColor' => 'rgba(59,89,153 ,1 )',
-            'pointHoverBorderColor'     => 'rgba(59,89,153 ,1 )',
+            'backgroundColor'           => 'rgba(59,89,153 ,0.5 )',
+            'borderColor'               => 'rgba(59,89,153 ,0.5 )',
+            'pointHoverBackgroundColor' => 'rgba(59,89,153 ,0.5 )',
+            'pointHoverBorderColor'     => 'rgba(59,89,153 ,0.5 )',
         ],
         'snapchat' => [
-            'backgroundColor'           => 'rgba(255,252,0 ,1)',
-            'borderColor'               => 'rgba(255,255,255 ,1)',
-            'pointHoverBackgroundColor' => 'rgba(255,252,0 ,1)',
-            'pointHoverBorderColor'     => 'rgba(255,252,0 ,1)',
+            'backgroundColor'           => 'rgba(255,252,0 ,0.5)',
+            'borderColor'               => 'rgba(255,255,255 ,0.5)',
+            'pointHoverBackgroundColor' => 'rgba(255,252,0 ,0.5)',
+            'pointHoverBorderColor'     => 'rgba(255,252,0 ,0.5)',
         ],
         'bing' => [
-            'backgroundColor'           => 'rgb(12, 132, 132)',
-            'borderColor'               => 'rgb(51, 51, 51)',
-            'pointHoverBackgroundColor' => 'rgba(51,170,51,0.75)',
-            'pointHoverBorderColor'     => 'rgba(51,170,51,1)',
+            'backgroundColor'           => 'rgba(0, 255, 0, 0.5)',
+            'borderColor'               => 'rgba(0, 255, 0, 0.5)',
+            'pointHoverBackgroundColor' => 'rgba(0, 255, 0, 0.5)',
+            'pointHoverBorderColor'     => 'rgba(0, 255, 0, 0.5)',
         ],
         'google' => [
-            'backgroundColor'           => 'rgba(51,170,51,0.1)',
-            'borderColor'               => 'rgba(51,170,51,0.8)',
-            'pointHoverBackgroundColor' => 'rgba(51,170,51,0.75)',
-            'pointHoverBorderColor'     => 'rgba(51,170,51,1)',
+            'backgroundColor'           => 'rgba(221,75,57 ,0.5)',
+            'borderColor'               => 'rgba(221,75,57 ,0.5)',
+            'pointHoverBackgroundColor' => 'rgba(221,75,57 ,0.5)',
+            'pointHoverBorderColor'     => 'rgba(221,75,57 ,0.5)',
         ],
     ];
 
